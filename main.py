b7b533a274fe8b0376ee1f2d8f3d8dd0dae962e4
Guru-CS <gurusaran.cs@gmail.com>
2026-02-13 10:43:30 -0500

    First

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-dwdwdddssdsdwds
-ss
-setdss
-sorted
+try:
+    with open("NovelTrackerFile",'r') as file:
+        print("Opening Existing File")
+except :
+    with open ("NovelTrackerFile",'w') as file:
+        print("New File Is Made")
+
+def main():
+
